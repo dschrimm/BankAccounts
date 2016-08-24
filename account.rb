cd33075b7bc961eb26d714baf646d5027b234a5a
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-08-24 14:53:06 -0700

    Meets basic requirements for Wave 2: reads CSV, returns collection of Account instances, returns an instance of Account where id field matches passed parameter.

--- account.rb
+++ account.rb
@@ -1,12 +1,14 @@
+require 'csv'
 require_relative 'owner'
 
 module Bank
   class Account
-    attr_accessor :id, :balance
+    attr_accessor :id, :balance, :date, :accounts
 
-    def initialize(id, balance)
+    def initialize(id, balance, date = nil)
       @id = id
       @balance = balance
+      @date = date
 
       unless @balance >= 0
         raise ArgumentError.new("A new account cannot be created with initial negative balance.")
@@ -32,9 +34,34 @@ module Bank
       return @balance
     end
 
+    # return collection of Account instances
+    def self.all
+      accounts = []
+      CSV.read('support/accounts.csv').each do |line|
+        accounts << self.new(line[0].to_i, line[1].to_i, line[2])
+      end
+      return accounts
+    end
+
+    # returns an instance of Account where value of the id matches the parameter
+    def self.find(id)
+      self.all.each do |i|
+        if i.id == id
+          return i
+        end
+      end
+      puts "No account with this id number."
+    end
+
   end
 end
 
 # a = Bank::Account.new(1234, 40)
 # o = a.add_owner("danielle", "909 green way", 5555555)
 # o.owner_info
+# a.deposit(10)
+# a.balance
+# a.withdraw(60)
+# a.balance
+# a.withdraw(30)
+# a.balance
