f37ddb461cb48d286858c589a2ccaf21e0fc5ee3
Danielle Schrimmer <danielle.schrimmer@gmail.com>
2016-08-26 10:20:01 -0700

    Add overdraft constat

--- checkingaccount.rb
+++ checkingaccount.rb
@@ -2,6 +2,8 @@ require_relative 'account'
 
 class CheckingAccount < Bank::Account
   TRANSACTION_FEE = 100 # cents
+  CHECK_WITHDRAWL_FEE = 200 # cents
+  MAX_OVERDRAFT = 1000 # cents
 
   attr_accessor :checks_remaining
 
@@ -21,19 +23,19 @@ class CheckingAccount < Bank::Account
     return @balance
   end
 
-  # withdrawl with $2 fee if more than 3 checks used, cannot withdraw more than $10 below current balance
+  # withdrawl fee activated if more than 3 checks used, overdraft limit
   def withdraw_using_check(amount)
-    amount += 200 if @checks_remaining <= 0
-    if amount <= @balance + 1000
+    amount += CHECK_WITHDRAWL_FEE if @checks_remaining <= 0
+    if amount <= @balance + MAX_OVERDRAFT
       @balance -= amount
       @checks_remaining -= 1
     else
-      puts "Your account does not contain enough to withdraw the amount requested. You may not overdraft more than $10."
+      puts "Your account does not contain enough to withdraw the amount requested. You may not overdraft more than $#{ '%.2f' % (MAX_OVERDRAFT / 100.0)}."
     end
     return @balance
   end
 
-  # resets check count to avoid $2 transaction fee
+  # resets check count to avoid transaction fee
   def reset_checks
     @checks_remaining = 3
   end
